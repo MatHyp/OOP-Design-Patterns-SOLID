4d6de8f10a5ce45cc6f24b41b11ad151defc2a8f
MatHyp <matijoker29@gmail.com>
2025-09-06 09:33:41 +0200

    Encapsulation

---
+++ src/OOP/Encapsulation/BankAccount.cs
@@ -0,0 +1,48 @@
+// Encapsulation means wrapping data (fields/properties) and the methods (functions) 
+// that operate on that data into a single unit — usually a class. 
+// It also restricts direct access to some components of an object 
+// to protect the internal state.
+
+
+using System;
+
+public class BankAccount
+{
+    private decimal balance;
+
+
+    public BankAccount(decimal balance)
+    {
+        this.balance = balance;
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be positive");
+        }
+
+
+        this.balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdraw amount must be positive");
+        }
+        if() (amount > balance)
+        {
+            throw new InvalidOperationException("Insufficient funds");
+        }
+
+        balance -= amount;
+    }
+}
