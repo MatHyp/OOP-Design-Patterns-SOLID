312a1c76b1a6127bdc48c05e00fc3ad7f82caaea
MatHyp <matijoker29@gmail.com>
2025-09-08 17:18:10 +0200

    SOLID L

---
+++ src/SOLID/L/BadExample/Shape.cs
@@ -0,0 +1,9 @@
+
+
+namespace SOLID.L.BadExample
+{
+    public abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+}
---
+++ src/SOLID/L/BetterExample/Rectangle.cs
@@ -0,0 +1,10 @@
+namespace SOLID.L.BetterExample
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area => Width * Height;
+    }
+}
